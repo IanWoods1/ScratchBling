00402c96cdbade61e492024cd503093988a14385
Ian Woods <iwoods@DESKTOP-IQIMCTS.localdomain>
2021-06-20 10:40:55 -0400

    Removed unused code from db_connect.js.

--- Backend/db_connect.js
+++ Backend/db_connect.js
@@ -1,19 +1,12 @@
 const Pool = require("pg").Pool;
-
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "password123",
-//   database: "test_scratch_bling",
-//   host: "localhost", 
-//   port: 5432
-// });
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  password: "6WCgVNUmPESK",
-  database: "scratch_bling_rds",
-  host: "database-1.ccazdsjz3qrl.us-east-2.rds.amazonaws.com", 
-  port: 5432
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST, 
+  port: process.env.PGPORT
 });
 
 module.exports = pool;
